aa5ef5e0fc8130c51da276747ff6929c27c7f4a3
Souhardaya Saha <90009320+Souhardaya@users.noreply.github.com>
2023-06-24 23:45:44 +0530

    Added solution - LeetHub

--- Sort an array of 0s, 1s and 2s - GFG/sort-an-array-of-0s-1s-and-2s.java
+++ Sort an array of 0s, 1s and 2s - GFG/sort-an-array-of-0s-1s-and-2s.java
@@ -8,13 +8,32 @@ import java.util.*;
 // } Driver Code Ends
 //User function template for Java
 
-class Solution
-{
-    public static void sort012(int a[], int n)
-    {
-        Arrays.sort(a);
+
+class Solution {
+    public static void sort012(int a[], int n) {
+        int low = 0;
+        int mid = 0;
+        int high = n - 1;
+        
+        while (mid <= high) {
+            if (a[mid] == 0) {
+                int temp = a[low];
+                a[low] = a[mid];
+                a[mid] = temp;
+                low++;
+                mid++;
+            } else if (a[mid] == 1) {
+                mid++;
+            } else {
+                int temp = a[mid];
+                a[mid] = a[high];
+                a[high] = temp;
+                high--;
+            }
+        }
     }
 }
+
 
 //{ Driver Code Starts.
 
