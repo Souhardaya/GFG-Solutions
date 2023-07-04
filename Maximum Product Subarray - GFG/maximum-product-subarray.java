cba243092d022edf49ab0002ef1eb2d2d5c84d9f
Souhardaya Saha <90009320+Souhardaya@users.noreply.github.com>
2023-07-05 01:45:29 +0530

    Added solution - LeetHub

---
+++ Maximum Product Subarray - GFG/maximum-product-subarray.java
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+  public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine());
+        while (tc-- > 0) {
+            int n = Integer.parseInt(br.readLine());
+            int[] arr = new int[n];
+            String[] inputLine = br.readLine().split(" ");
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+
+            System.out.println(new Solution().maxProduct(arr, n));
+        }
+    }
+}
+
+// } Driver Code Ends
+
+class Solution {
+    // Function to find maximum product subarray
+    long maxProduct(int[] arr, int n) {
+        long pre = 1;
+        long suff = 1;
+        long ans = Long.MIN_VALUE;
+        
+        for (int i = 0; i < n; i++) {
+            if (pre == 0)
+                pre = 1;
+            if (suff == 0)
+                suff = 1;
+            
+            pre *= arr[i];
+            suff *= arr[n - i - 1];
+            
+            ans = Math.max(ans, Math.max(pre, suff));
+        }
+        
+        return ans;
+    }
+}
