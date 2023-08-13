e188ed2c95f9f0ab02d3eb3b5337ace01704fff9
Souhardaya Saha <90009320+Souhardaya@users.noreply.github.com>
2023-08-13 12:11:41 +0530

    Added solution - LeetHub

---
+++ Word Search - GFG/word-search.java
@@ -0,0 +1,82 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            String[] S = br.readLine().trim().split(" ");
+            int n = Integer.parseInt(S[0]);
+            int m = Integer.parseInt(S[1]);
+            char [][] board = new char[n][m];
+            for(int i = 0; i < n; i++){
+                String[] s = br.readLine().trim().split(" ");
+                for(int j = 0; j < m; j++){
+                    board[i][j] = s[j].charAt(0);
+                }
+            }
+            String word = br.readLine().trim();
+            Solution obj = new Solution();
+            boolean ans = obj.isWordExist(board, word);
+            if(ans)
+                System.out.println("1");
+            else
+                System.out.println("0");
+       }
+    }
+}
+// } Driver Code Ends
+
+
+
+
+class Solution {
+    private int ROWS, COLS;
+    private Set<int[]> path;
+
+    public boolean isWordExist(char[][] board, String word) {
+        ROWS = board.length;
+        COLS = board[0].length;
+        path = new HashSet<>();
+        for (int r = 0; r < ROWS; r++) {
+            for (int c = 0; c < COLS; c++) {
+                if (findWords(board, word, r, c, 0)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private boolean findWords(char[][] board, String word, int r, int c, int i) {
+        if (i == word.length()) {
+            return true;
+        }
+        if (r < 0 || c < 0 || r >= ROWS || c >= COLS || word.charAt(i) != board[r][c] || containsCoordinate(path, r, c)) {
+            return false;
+        }
+        path.add(new int[]{r, c});
+
+        boolean res = (findWords(board, word, r + 1, c, i + 1) ||
+                findWords(board, word, r - 1, c, i + 1) ||
+                findWords(board, word, r, c + 1, i + 1) ||
+                findWords(board, word, r, c - 1, i + 1));
+        path.removeIf(coord -> coord[0] == r && coord[1] == c);
+        return res;
+    }
+
+    private boolean containsCoordinate(Set<int[]> coordinates, int r, int c) {
+        for (int[] coord : coordinates) {
+            if (coord[0] == r && coord[1] == c) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
