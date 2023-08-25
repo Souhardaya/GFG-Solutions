c49f90102b38b66905b4611b54937aff05f635ee
Souhardaya Saha <90009320+Souhardaya@users.noreply.github.com>
2023-08-25 13:49:14 +0530

    Added solution - LeetHub

---
+++ Trapping Rain Water - GFG/trapping-rain-water.java
@@ -0,0 +1,63 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+
+
+class Array {
+
+	public static void main (String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine().trim()); //Inputting the testcases
+		while(t-->0){
+		  
+		    //size of array
+		    int n = Integer.parseInt(br.readLine().trim());
+		    int arr[] = new int[n];
+		    String inputLine[] = br.readLine().trim().split(" ");
+		    
+		    //adding elements to the array
+		    for(int i=0; i<n; i++){
+		        arr[i] = Integer.parseInt(inputLine[i]);
+		    }
+		    
+		    Solution obj = new Solution();
+		    
+		    //calling trappingWater() function
+		    System.out.println(obj.trappingWater(arr, n));
+		}
+	}
+}
+
+
+// } Driver Code Ends
+
+
+class Solution {
+    
+    // arr: input array
+    // n: size of array
+    // Function to find the trapped water between the blocks.
+    static long trappingWater(int arr[], int n) { 
+        long[] prefix = new long[n];
+        long[] suffix = new long[n];
+        
+        prefix[0] = arr[0]; // Fixed typo: using arr[0] instead of trappingWater[0]
+        for (int i = 1; i < n; i++) {
+            prefix[i] = Math.max(prefix[i - 1], arr[i]);
+        }
+        
+        suffix[n - 1] = arr[n - 1]; // Fixed typo: using arr[n - 1] instead of trappingWater[n - 1]
+        for (int i = n - 2; i >= 0; i--) {
+            suffix[i] = Math.max(suffix[i + 1], arr[i]);
+        }
+        
+        long trappedWater = 0;
+        for (int i = 0; i < n; i++) {
+            trappedWater += Math.min(prefix[i], suffix[i]) - arr[i];
+        }
+        
+        return trappedWater;
+    } 
+}
+
